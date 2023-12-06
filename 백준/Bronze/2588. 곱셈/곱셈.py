cefa4c3b6684f395d29bd11ea427fe3c197155bf
condatos <152856364+newdatajaam@users.noreply.github.com>
2023-12-06 18:44:33 +0900

    [Bronze III] Title: 곱셈, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/2588. 곱셈/곱셈.py
@@ -0,0 +1,12 @@
+num1 = input()
+num2 = input()
+
+level2 = int(num1)*int(num2[2])
+level3 = int(num1)*int(num2[1])
+level4 = int(num1)*int(num2[0])
+answer = int(num1)*int(num2)
+
+print(level2)
+print(level3)
+print(level4)
+print(answer)
