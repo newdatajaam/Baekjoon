438c7986e4c8c2db76bd9cf6f14f0f72ac781a31
condatos <152856364+newdatajaam@users.noreply.github.com>
2023-12-08 15:27:01 +0900

    [Bronze III] Title: 암호제작, Time: 320 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/1837. 암호제작/암호제작.py
@@ -0,0 +1,18 @@
+num,k = map(int,input().split())
+cnt = 2
+
+for sosu in range(2,k):
+  # num안에 k보다 작은 소수가 있다면
+  if num%sosu == 0:
+    p = sosu
+    q = int(num / p)
+    # 2부터 for문이 돌기 때문에 p는 무조건 q보다 작은 숫자이기때문에
+    print("BAD", p)
+    break
+  # num이 k 보다 작은 소수가 없다면
+  else:
+    cnt += 1
+
+
+if cnt >= k:
+  print("GOOD")
