b7529cc0be48afcc46a2cd9a62a01edcaa8b2307
condatos <152856364+newdatajaam@users.noreply.github.com>
2023-12-08 15:30:03 +0900

    [Bronze III] Title: 암호제작, Time: 312 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/1837. 암호제작/암호제작.py
+++ 백준/Bronze/1837. 암호제작/암호제작.py
@@ -1,11 +1,10 @@
 num,k = map(int,input().split())
 cnt = 2
 
-for sosu in range(2,k):
+for sosu in range(2,k): # 소수를 찾으려면 2 이상이어야 함
   # num안에 k보다 작은 소수가 있다면
   if num%sosu == 0:
     p = sosu
-    q = int(num / p)
     # 2부터 for문이 돌기 때문에 p는 무조건 q보다 작은 숫자이기때문에
     print("BAD", p)
     break
@@ -13,6 +12,6 @@ for sosu in range(2,k):
   else:
     cnt += 1
 
-# k(=cnt)번 만큼 
+# k(=cnt)번 만큼 돌았는데도 소수가 없다면 k보다 큰 소수 인 것이므로 good출력
 if cnt == k:
   print("GOOD")
