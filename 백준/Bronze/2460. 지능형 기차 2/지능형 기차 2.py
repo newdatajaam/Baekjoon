dfa4ceb1b11d9a6e1ff9fb7553db8241b60a555c
condatos <152856364+newdatajaam@users.noreply.github.com>
2023-12-11 09:33:03 +0900

    [Bronze III] Title: 지능형 기차 2, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/2460. 지능형 기차 2/지능형 기차 2.py
@@ -0,0 +1,14 @@
+train = 0
+people = []
+
+for station in range(0,10):
+  get_out, get_in = map(int, input().split())
+  train -= get_out
+  if train < 0:
+    print("오류")
+  train += get_in
+  if train > 10000:
+    print("정원초과")
+  people.append(train)
+
+print(max(people))
