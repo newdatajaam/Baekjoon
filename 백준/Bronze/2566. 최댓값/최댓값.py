bbc762cbc3907b98298e4679375946c32d382f1a
condatos <152856364+newdatajaam@users.noreply.github.com>
2023-12-15 13:38:52 +0900

    [Bronze III] Title: 최댓값, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2566. 최댓값/최댓값.py
+++ 백준/Bronze/2566. 최댓값/최댓값.py
@@ -1,22 +1,21 @@
-row = []
-row_max = 0
-row_count = 0
+max_num = 0
 column_count = 0
+row_max = 0
 column_max = 0
 
 # 1열 부터 9열 반복 (편의상 1부터 10으로)
 for i in range(1,10):
   row = list(map(int, input().split()))
 
-  if max(row) >= row_max:
-    row_max = max(row)
-    row_count = i
+  if max(row) >= max_num:
+    max_num = max(row)
+    row_max = i
 
     for num in row:
       column_count += 1
-      if row_max == num:
+      if max_num == num:
         column_max = column_count
   column_count = 0
 
-print(row_max)
-print(row_count, column_max)
+print(max_num)
+print(row_max, column_max)
