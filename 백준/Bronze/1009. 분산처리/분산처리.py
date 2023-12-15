2445077de15b7652afc9f6a135cae762f9d0018d
condatos <152856364+newdatajaam@users.noreply.github.com>
2023-12-15 18:54:18 +0900

    [Bronze II] Title: 분산처리, Time: 76 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/1009. 분산처리/분산처리.py
+++ 백준/Bronze/1009. 분산처리/분산처리.py
@@ -1,15 +1,21 @@
+num_list = [[1,1,1,1],[2,4,8,6],[3,9,7,1],[4,6,4,6],[5,5,5,5],[6,6,6,6],[7,9,3,1],[8,4,2,6],[9,1,9,1]]
+
 test = int(input())
 
 for i in range(test):
-  a, b = map(int,input().split())
-  if b % 4 == 0:
+  a,b = map(int,input().split())
+
+  if a % 10 == 0:
+    a = 10
+  else:
+    a = a%10
+
+  if b%4 == 0:
     b = 4
-  else: 
-    b = b % 4
+  else:
+    b = b%4
   
-  num = a**b
-  
-  if num % 10 == 0:
+  if a == 10:
     print(10)
   else:
-    print(num%10)
+    print(num_list[a-1][b-1])
