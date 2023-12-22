5187d58b77259d1c4a39bca06be8f0d46abbf9a1
condatos <152856364+newdatajaam@users.noreply.github.com>
2023-12-22 22:07:17 +0900

    [Bronze II] Title: 운동, Time: 48 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/1173. 운동/운동.py
+++ 백준/Bronze/1173. 운동/운동.py
@@ -5,19 +5,19 @@ min_T = 0
 
 X = m               # 영식이의 초기 맥박
 
-if m + T > M:
-  min_T = -1
-else:
-  while excercise_T < N:
-    if X + T <= M:
-      X += T
-      excercise_T += 1
-      min_T += 1
-    else:
-      X -= R
-      min_T += 1
-      if X < m:
-        X = m
 
+while excercise_T < N: 
+  if m + T > M:
+    min_T = -1
+    break   
+  if X + T <= M:
+    X += T
+    excercise_T += 1
+    min_T += 1
+  else:
+    X -= R
+    min_T += 1
+    if X < m:
+      X = m
 
 print(min_T)
