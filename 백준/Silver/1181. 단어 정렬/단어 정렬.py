bd0c2e3f0383f1a32ee50b186725cdd9bb349925
condatos <152856364+newdatajaam@users.noreply.github.com>
2024-01-30 12:40:28 +0900

    [Silver V] Title: 단어 정렬, Time: 92 ms, Memory: 35932 KB -BaekjoonHub

--- 백준/Silver/1181. 단어 정렬/단어 정렬.py
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.py
@@ -1,10 +1,9 @@
 import sys
 n = int(sys.stdin.readline())
-data = [sys.stdin.readline().strip() for i in range(n)]
+data = [sys.stdin.readline().strip() for i in range(n)]  # 단어를 리스트로 받음
 word_dict = {}
 
-for d in data: # 나중에 data로 바꿔야함
-  word = d
+for word in data:
   word_dict[word] = len(word)     # 딕셔너리로 {단어 : 단어 길이}입력 받음
 
 ans = sorted(word_dict.items(), key=lambda x: (x[1],x[0]))
