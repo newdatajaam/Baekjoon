db21cafeb99e889f606a0da4a4385e17d6ca7d07
condatos <152856364+newdatajaam@users.noreply.github.com>
2024-02-16 13:58:50 +0900

    [Silver V] Title: K-세준수, Time: 1388 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver V/1418. K－세준수/K－세준수.py
+++ 백준/Silver V/1418. K－세준수/K－세준수.py
@@ -5,17 +5,17 @@ def k_SejunNum(n):
     i = 2
     while i <= n:
       if i > k:
-         return 0
-      if n % i == 0:
-         n = n / i
-         if n == 1:
-            return 1
-      else:
-         i = i + 1
+         return 0   
 
+      if n % i == 0:  
+         n = n / i  
+      else:
+         i = i + 1  
+         
+    return 1
 
 cnt = 0
-for N in range(2, num+1): # 문제 오류인듯 소수를 1부터 카운트해야 일치
+for N in range(2, num+1):
  if k_SejunNum(N):
   cnt += 1
 print(cnt+1)
