f62e559f8d8229f5f04d65b613e7d8f530a668b1
condatos <152856364+newdatajaam@users.noreply.github.com>
2024-02-22 20:43:35 +0900

    [Silver V] Title: 누울 자리를 찾아라, Time: 48 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1652. 누울 자리를 찾아라/누울 자리를 찾아라.py
+++ 백준/Silver/1652. 누울 자리를 찾아라/누울 자리를 찾아라.py
@@ -1,45 +1,36 @@
 n = int(input())
 bed = []
-cnt_col = 0
-cnt_row = 0
+cnt_col,cnt_row = 0, 0
 chk_list = []
 
 for _ in range(n):
   bed.append(list(input()))
 
+def cnt_def(list_name):
+  cnt = 0
+  if len(list_name) == 0:
+    pass
+  elif len(list_name) == 1: 
+    cnt +=1
+  else:
+    cnt += 1
+    for num in range(len(list_name)-1):
+      if list_name[num]+1 != list_name[num+1]:
+        cnt +=1
+  return cnt
+
 for i in range(n):
   for j in range(n-1):
     if bed[i][j] == '.' and bed[i][j] == bed[i][j+1]:
       chk_list.append(j)
-
-  if len(chk_list) == 0:
-    pass
-  elif len(chk_list) == 1: 
-    cnt_row +=1
-  else:
-    cnt_row += 1
-    for num in range(len(chk_list)-1):
-      if chk_list[num]+1 != chk_list[num+1]:
-        cnt_row +=1
-        #print(cnt_row)
-  #print('*'*30)
+  cnt_row += cnt_def(chk_list)
   chk_list = []
 
 for j in range(n):
   for i in range(n-1):
     if bed[i][j] == '.' and bed[i][j] == bed[i+1][j]:
       chk_list.append(i)
-  
-  #print(chk_list)
-  if len(chk_list) == 0:
-    pass
-  elif len(chk_list) == 1: 
-    cnt_col +=1
-  else:
-    cnt_col += 1
-    for num in range(len(chk_list)-1):
-      if chk_list[num]+1 != chk_list[num+1]:
-        cnt_col +=1
+  cnt_col += cnt_def(chk_list)
   chk_list = []
   
 print(cnt_row, cnt_col)
