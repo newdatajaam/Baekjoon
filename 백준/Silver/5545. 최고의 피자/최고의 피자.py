20a340ca64544537fd184663197640760ccd5f28
condatos <152856364+newdatajaam@users.noreply.github.com>
2024-03-25 11:02:37 +0900

    [Silver III] Title: 최고의 피자, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/5545. 최고의 피자/최고의 피자.py
+++ 백준/Silver/5545. 최고의 피자/최고의 피자.py
@@ -8,9 +8,6 @@ best_kcal = int(dough_kcal / dough_price)
 for num in range(1, num_topping+1):
   tot_price = dough_price + topping_price * num
   tot_kcal = dough_kcal + sum(topping_kcal[0:num])
-
-  #print(num, tot_kcal, tot_price, best_kcal)
-
   if tot_kcal / tot_price > best_kcal:
     best_kcal = int(tot_kcal / tot_price)
 print(best_kcal)
