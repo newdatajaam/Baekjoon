e883758a6c577d5e4969a27f423e005e2c48d903
condatos <152856364+newdatajaam@users.noreply.github.com>
2024-09-04 17:03:27 +0900

    [level 2] Title: 전화번호 목록, Time: 94.71 ms, Memory: 30.6 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
@@ -1,22 +1,12 @@
 def solution(phone_book):
-    answer = True
     phone_book.sort()
     
     for i in range(len(phone_book)-1):
         if phone_book[i+1].startswith(phone_book[i]):
-            answer = False
-            break
-    return answer
+            return False
+            
+    return True
 
-# def solution(phone_book):
-#     answer = True
-#     phone_book = sorted(phone_book, key= lambda x: len(x))
-    
-#     for i in range(len(phone_book)-1):
-#         for j in range(i+1, len(phone_book)):
-#             if phone_book[j][:len(phone_book[i])] == phone_book[i]:
-#                 return False
-#     return True
 
 # def solution(phone_book):
 #     answer = True
@@ -27,5 +17,6 @@ def solution(phone_book):
 #             l = len(phone_book[i])
 #             if phone_book[i] == phone_book[j][0:l]:
 #                 answer = False
+#                 break
     
 #     return answer
