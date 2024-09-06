a0e0758606e8241a92099ff45c2fc81aab6fdb19
condatos <152856364+newdatajaam@users.noreply.github.com>
2024-09-06 15:57:41 +0900

    [level 1] Title: 모의고사, Time: 4.23 ms, Memory: 10.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/42840. 모의고사/모의고사.py
@@ -0,0 +1,22 @@
+def solution(answers):
+    answer = []
+    stu1 = [1,2,3,4,5]
+    stu2 = [2,1,2,3,2,4,2,5]
+    stu3 = [3,3,1,1,2,2,4,4,5,5]
+    scores = [0,0,0]
+    
+    for i in range(len(answers)):
+        ans = answers[i]
+        if ans == stu1[((i+1)%(len(stu1)))-1]:
+            scores[0] += 1
+        if ans == stu2[((i+1)%(len(stu2)))-1]:
+            scores[1] += 1
+        if ans == stu3[((i+1)%(len(stu3)))-1]:
+            scores[2] += 1
+    
+    for j in range(len(scores)):
+        if scores[j] == max(scores):
+            answer.append(j+1)
+    
+    return answer
+
